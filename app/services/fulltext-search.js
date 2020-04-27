c24c1965d7318675957e9a30927efc962b5b372f
InsertIntoHTL <5125@htl.rennweg.at>
2020-04-27 12:02:37 +0200

    fulltext-search erweitern

--- app/services/fulltext-search.js
+++ app/services/fulltext-search.js
@@ -24,18 +24,41 @@ app.service("FulltextSearch", function ($log, $timeout) {
 
 
     const options = {
+
         includeScore: true,
         shouldSort: true,
         findAllMatches: true,
         keys: ['author', 'tags']
+
     };
 
     const fuse = new Fuse(root_list, options);
 
     this.search = (input) => {
-        this.output = fuse.search(input);
+
         console.log('%c School-Output', 'color:orange; font-size: 32px;');
-        console.table(this.output);
-        return this.output;
+
+        if(input === "") {
+            return this.createListWithoutInput();
+        } else {
+            this.output = fuse.search(input);
+            console.table(this.output);
+            return this.output;
+        }
+
+
     };
+
+    this.createListWithoutInput = () => {
+        let list = [];
+        root_list.forEach(n => {
+            list.push(
+                {
+                    "item" : n
+                }
+            )
+        });
+        console.table(list);
+        return list;
+    }
 });
