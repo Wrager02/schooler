66ba675c1e1103256932411b97420be256cbb20c
Wrager02 <alre0209@gmail.com>
2020-05-26 18:46:00 +0200

    Expand WIP detail-view CSS

--- app/components/detail-view.js
+++ app/components/detail-view.js
@@ -25,8 +25,9 @@ app.controller("DetailViewController", function ($log, $stateParams, FilterByIdS
 
     FilterByIdService.filterByID(this.id).then(response => {
         this.school = response[0];
-        document.getElementById("map").innerHTML =
-            "<iframe frameborder=\"0\" style=\"border:0;\" src=\"https://www.google.com/maps/embed/v1/place?q=" + this.school.name + "," + this.school.city+ "&key=AIzaSyBpl8WLJuPi3SuXpXezbMuZeubiaozm2gM\" allowfullscreen></iframe>"
+        let map = document.getElementById("map");
+        map.innerHTML =
+            "<iframe width='" + map.clientWidth + "' height='" + map.clientHeight + "' frameborder='0' style='border:0; border-radius: 8%;' src='https://www.google.com/maps/embed/v1/place?q=" + this.school.name + "," + this.school.city+ "&key=AIzaSyBpl8WLJuPi3SuXpXezbMuZeubiaozm2gM' allowfullscreen></iframe>"
         ;
     });
 
