5e7bedc2ba685c007e7d77346aa0629b3b899837
InsertIntoHTL <5125@htl.rennweg.at>
2020-06-15 21:57:57 +0200

    wir sind einfach besser als ihr

---
+++ app/services/save-filter.js
@@ -0,0 +1,33 @@
+"use strict";
+
+app.service("SaveFilter", function ($log) {
+
+    $log.debug("SaveFilter()");
+
+    this.saveFilter = (selectedTags, control, input) => {
+        localStorage.setItem("selectedTags", JSON.stringify(selectedTags));
+        localStorage.setItem("control", JSON.stringify(control));
+        localStorage.setItem("input", JSON.stringify(input));
+    };
+
+    this.loadSelectedTags = () => {
+        if(localStorage.getItem("selectedTags") !== "undefined") {
+            return JSON.parse(localStorage.getItem("selectedTags"));
+        }
+        return [];
+    };
+
+    this.loadControl = () => {
+        if(localStorage.getItem("control") !== "undefined") {
+            return JSON.parse(localStorage.getItem("control"));
+        }
+        return [false, false, false, false, false, false, false];
+    };
+
+    this.loadInput = () => {
+        if(localStorage.getItem("input") !== "undefined") {
+            return JSON.parse(localStorage.getItem("input"));
+        }
+        return "";
+    };
+});
