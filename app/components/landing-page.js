e58c3956c5581bebd9f0690aa06e048671c4ec4e
Tim Schreiber <tim.schreiber@outlook.com>
2020-06-23 15:20:15 +0200

    made the navbar change colors

--- app/components/landing-page.js
+++ app/components/landing-page.js
@@ -34,6 +34,19 @@ app.controller("LandingPageController", function ($log) {
     this.scrollInfo = () => {
         document.querySelector('.landing-second').scrollIntoView({ behavior: 'smooth' });
         console.log("lost");
+    };
+
+    var element_position = $('.landing-second').offset().top;
+
+    $(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = element_position;
+
+    if(y_scroll_pos > scroll_pos_test) {
+        $('#burger-ul').addClass('dark-nav');
+    }else{
+        $('#burger-ul').removeClass('dark-nav');
     }
+});
 
 });
